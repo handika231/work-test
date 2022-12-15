7db1ce0c878186a7cca4d782bad7755013cd1ddd
Handika Rafli A <handika.aryatama@gmail.com>
2022-12-15 12:33:05 +0700

    solve 3 a

--- lib/controller/calculator_controller.dart
+++ lib/controller/calculator_controller.dart
@@ -2,11 +2,90 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CalculatorController extends GetxController {
-  final formKey = GlobalKey<FormState>();
+  final formKey = GlobalKey<FormState>(debugLabel: 'calculatorForm');
   final TextEditingController firstNumberController = TextEditingController();
   final TextEditingController secondNumberController = TextEditingController();
   final TextEditingController thirdNumberController = TextEditingController();
 
-  final isChecked = false.obs;
+  final isFirstChecked = false.obs;
+  final isSecondChecked = false.obs;
+
+  final isThirdChecked = false.obs;
+
   final result = 0.obs;
+
+  int increment() {
+    //jika yang di ceklis bernilai true maka akan di tambah
+    result.value = 0;
+    if (isFirstChecked.value) {
+      result.value += int.parse(firstNumberController.text);
+    }
+    if (isSecondChecked.value) {
+      result.value += int.parse(secondNumberController.text);
+    }
+    if (isThirdChecked.value) {
+      result.value += int.parse(thirdNumberController.text);
+    }
+    return result.value;
+  }
+
+  int decrement() {
+    result.value = 0;
+    int firstNumber = 0;
+    int secondNumber = 0;
+    int thirdNumber = 0;
+    if (isFirstChecked.value) {
+      firstNumber = int.parse(firstNumberController.text);
+    }
+    if (isSecondChecked.value) {
+      secondNumber = int.parse(secondNumberController.text);
+    }
+    if (isThirdChecked.value) {
+      thirdNumber = int.parse(thirdNumberController.text);
+    }
+    result.value = firstNumber - secondNumber - thirdNumber;
+    return result.value;
+  }
+
+  int multiply() {
+    result.value = 0;
+    int firstNumber = 1;
+    int secondNumber = 1;
+    int thirdNumber = 1;
+    if (isFirstChecked.value) {
+      firstNumber =
+          isFirstChecked.value ? int.parse(firstNumberController.text) : 1;
+    }
+    if (isSecondChecked.value) {
+      secondNumber =
+          isSecondChecked.value ? int.parse(secondNumberController.text) : 1;
+    }
+    if (isThirdChecked.value) {
+      thirdNumber =
+          isThirdChecked.value ? int.parse(thirdNumberController.text) : 1;
+    }
+    result.value = firstNumber * secondNumber * thirdNumber;
+    return result.value;
+  }
+
+  int divide() {
+    result.value = 0;
+    double firstNumber = 1;
+    double secondNumber = 1;
+    double thirdNumber = 1;
+    if (isFirstChecked.value) {
+      firstNumber =
+          isFirstChecked.value ? double.parse(firstNumberController.text) : 1;
+    }
+    if (isSecondChecked.value) {
+      secondNumber =
+          isSecondChecked.value ? double.parse(secondNumberController.text) : 1;
+    }
+    if (isThirdChecked.value) {
+      thirdNumber =
+          isThirdChecked.value ? double.parse(thirdNumberController.text) : 1;
+    }
+    result.value = firstNumber / secondNumber ~/ thirdNumber;
+    return result.value;
+  }
 }
--- lib/views/calculator_view.dart
+++ lib/views/calculator_view.dart
@@ -11,30 +11,140 @@ class CalculatorView extends GetView<CalculatorController> {
       appBar: AppBar(
         title: const Text('Calculator View'),
       ),
-      body: ListView(
-        padding: const EdgeInsets.all(12),
-        children: [
-          CheckboxListTile(
-            contentPadding: EdgeInsets.zero,
-            title: TextFormField(
-              keyboardType: TextInputType.number,
-              controller: controller.firstNumberController,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(12),
+      body: Form(
+        key: controller.formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(12),
+          children: [
+            Obx(
+              () => CheckboxListTile(
+                contentPadding: EdgeInsets.zero,
+                title: TextFormField(
+                  keyboardType: TextInputType.number,
+                  controller: controller.firstNumberController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(12),
+                      ),
+                    ),
                   ),
                 ),
+                value: controller.isFirstChecked.value,
+                onChanged: (_) {
+                  controller.isFirstChecked.toggle();
+                },
               ),
             ),
-            value: controller.isChecked.value,
-            onChanged: (value) {},
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-        ],
+            const SizedBox(
+              height: 12,
+            ),
+            Obx(
+              () => CheckboxListTile(
+                contentPadding: EdgeInsets.zero,
+                title: TextFormField(
+                  keyboardType: TextInputType.number,
+                  controller: controller.secondNumberController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+                value: controller.isSecondChecked.value,
+                onChanged: (_) {
+                  controller.isSecondChecked.toggle();
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Obx(
+              () => CheckboxListTile(
+                selectedTileColor: Colors.red,
+                contentPadding: EdgeInsets.zero,
+                title: TextFormField(
+                  keyboardType: TextInputType.number,
+                  controller: controller.thirdNumberController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(12),
+                      ),
+                    ),
+                  ),
+                ),
+                value: controller.isThirdChecked.value,
+                onChanged: (_) {
+                  controller.isThirdChecked.toggle();
+                },
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            Wrap(
+              spacing: 10,
+              runSpacing: 10,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    _calculate(controller.increment());
+                  },
+                  child: const Text('+'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _calculate(controller.decrement());
+                  },
+                  child: const Text('-'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _calculate(controller.multiply());
+                  },
+                  child: const Text('x'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    _calculate(controller.divide());
+                  },
+                  child: const Text('/'),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Obx(
+              () => Text('Hasil: ${controller.result.value}'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void _calculate(int function) {
+    try {
+      if (controller.isFirstChecked.value && controller.isSecondChecked.value ||
+          controller.isFirstChecked.value && controller.isThirdChecked.value ||
+          controller.isSecondChecked.value && controller.isThirdChecked.value) {
+        function;
+      } else {
+        Get.snackbar(
+          'Error',
+          'Setidaknya 2 angka yang di ceklis',
+          snackPosition: SnackPosition.TOP,
+          backgroundColor: Colors.red,
+          colorText: Colors.white,
+        );
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
   }
 }
